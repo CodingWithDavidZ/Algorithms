0057506c77fb6c4348f06086409a93f32680be7c
David Zombar <davidzombar@gmail.com>
2022-06-13 14:55:28 -0600

    Added Breadth First Solution

--- Graph.js
+++ Graph.js
@@ -1,16 +1,69 @@
 // Data
-const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(" ");
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ');
 
 const routes = [
-    ['PHX', 'LAX'],
-    ['PHX', 'JFK'],
-    ['JFK', 'OKC']
-    ['JFK', 'HEL'],
-    ['HEL', 'MEX'],
-    ['MEX', 'EZE'],
-    ['EZE', 'BKK'],
-    ['BKK', 'LOS'],
-    ['LOS', 'LAP'],
-    ['LAP', 'LIM'],
-    ['LIM', 'OKC'],
+	['PHX', 'LAX'],
+	['PHX', 'JFK'],
+	['JFK', 'OKC'],
+	['JFK', 'HEL'],
+	['JFK', 'LOS'],
+	['MEX', 'LAX'],
+	['MEX', 'BKK'],
+	['MEX', 'LIM'],
+	['MEX', 'EZE'],
+	['LIM', 'BKK'],
 ];
+
+// Adjacency List
+const adjacencyList = new Map();
+
+//Add Node
+function addNode(airport) {
+	adjacencyList.set(airport, []);
+}
+
+//Add undirected Edge
+function addUndirectedEdge(origin, destination) {
+	adjacencyList.get(origin).push(destination);
+	adjacencyList.get(destination).push(origin);
+}
+
+//Create Graph
+airports.forEach(addNode);
+routes.forEach((route) => addUndirectedEdge(...route));
+
+console.log(adjacencyList);
+
+//Graph Traversal
+
+//BFS Breadth First Search
+
+function bfs(start) {
+    let counter = 0
+
+	const visited = new Set();
+
+	const queue = [start];
+
+	while (queue.length > 0) {
+		const airport = queue.shift(); // mutates queue
+
+		const destinations = adjacencyList.get(airport);
+
+		for (const destination of destinations) {
+            counter++;
+			if (destination === 'BKK') {
+				console.log(`BFS found Bangkok in ${counter} steps`);
+			}
+
+			if (!visited.has(destination)) {
+				visited.add(destination);
+				queue.push(destination);
+			}
+		}
+	}
+}
+
+bfs('PHX');
+
+
