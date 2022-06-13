f0fae3934d9574c32f5fe38343210e2fb7eb41e0
David Zombar <davidzombar@gmail.com>
2022-06-13 14:13:03 -0600

    Setup initial Data for Graph

---
+++ Graph.js
@@ -0,0 +1,16 @@
+// Data
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(" ");
+
+const routes = [
+    ['PHX', 'LAX'],
+    ['PHX', 'JFK'],
+    ['JFK', 'OKC']
+    ['JFK', 'HEL'],
+    ['HEL', 'MEX'],
+    ['MEX', 'EZE'],
+    ['EZE', 'BKK'],
+    ['BKK', 'LOS'],
+    ['LOS', 'LAP'],
+    ['LAP', 'LIM'],
+    ['LIM', 'OKC'],
+];
